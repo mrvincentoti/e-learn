16021e41d75e6fe509c36376679ed0daf3024986
vincentoti <vincent.oti@layer3.com.ng>
2022-07-21 12:29:12 +0100

    feature: created sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './css/ui-fonts.css';
+import './css/App.css';
+import './css/props.css';
+// screen
+import Header from './screens/header';
+import Sidebar from './screens/sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex">
+      <Sidebar />
+      <Header />
     </div>
   );
 }
